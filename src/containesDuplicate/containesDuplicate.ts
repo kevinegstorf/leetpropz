58edeaed9d02e03f07a686549cf71cab6458d1dd
Kevin Egstorf <kevin.egstorf@gmail.com>
2025-04-21 19:49:59 +0200

    feat: implement containsDuplicate function to check for duplicate numbers in an array

--- src/containesDuplicate/containesDuplicate.ts
+++ src/containesDuplicate/containesDuplicate.ts
@@ -1,1 +1,12 @@
-export const containsDuplicate = (nums: number[]) => undefined;
+export const containsDuplicate = (nums: number[]) => {
+  const seen = new Set<number>();
+
+  for (const num of nums) {
+    if (seen.has(num)) {
+      return true;
+    }
+    seen.add(num);
+  }
+
+  return false;
+};
